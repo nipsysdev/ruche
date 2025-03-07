fdb840ef536cf15fc25cb1cd51ef27518316b3ff
Xav <bonjour@xaviers.sh>
2025-03-07 01:25:46 -0500

    feature(initial_impl): initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::net::SocketAddr;
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on {}", addr);
+    
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
