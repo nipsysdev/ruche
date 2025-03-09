ff216947f90b13764f045cad3c643d2be1f7ae32
Xav <bonjour@xaviers.sh>
2025-03-09 17:26:07 -0400

    feature(initial_impl): wrap db in RwLock, register basic layers

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 mod models;
 mod services;
 
-use std::net::SocketAddr;
-use axum::{Router, routing::get};
 use crate::models::config::{parse_config, Config};
 use crate::services::db_service::DbService;
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use std::time::Duration;
+use tower::ServiceBuilder;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
+use tower_http::ServiceBuilderExt;
 
 #[derive(Clone)]
 struct AppState {
     config: Config,
-    db_service: DbService
+    db_service: DbService,
 }
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(TimeoutLayer::new(Duration::from_secs(15)))
+                .compression(),
+        )
         .with_state(AppState {
             config: parse_config().await,
-            db_service: DbService::init()
+            db_service: DbService::init(),
         });
-    
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    println!("listening on {}", addr);
-    
+    tracing::info!("Listening on {}", addr);
+
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
--- src/services/db_service.rs
+++ src/services/db_service.rs
@@ -1,15 +1,17 @@
 use std::sync::Arc;
 use polodb_core::Database;
+use tokio::sync::RwLock;
 
 #[derive(Clone)]
 pub struct DbService {
-    db: Arc<Database>,
+    db: Arc<RwLock<Database>>,
 }
 
 impl DbService {
     pub fn init() -> Self {
+        let db = Database::open_path("ruche.db").expect("Failed to open database");
         DbService {
-            db: Arc::new(Database::open_path("ruche.db").expect("Failed to open database"))
+            db: Arc::new(RwLock::new(db))
         }
     }
 }
