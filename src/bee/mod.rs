922bdfa2b0b7964db75fe709658279d576a35184
Xavier Saliniere <bonjour@xaviers.sh>
2025-03-13 07:03:48 -0400

    testing(bee): add tests on wip Bee::create function

--- src/bee/mod.rs
+++ src/bee/mod.rs
@@ -1,11 +1,12 @@
-use crate::services::db_service::DbService;
+use crate::services::db_service::BeeDatabase;
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Clone)]
 pub struct Bee {
-    id: u8,
-    neighborhood: String,
-    reserve_doubling: bool,
+    pub id: u8,
+    pub neighborhood: String,
+    pub reserve_doubling: bool,
 }
 
 impl Bee {
@@ -17,28 +18,148 @@ impl Bee {
         format!("node_{}", Self::format_id(id))
     }
 
-    pub async fn create(db: DbService) -> Result<Bee, String> {
+    pub async fn create(db: &dyn BeeDatabase) -> Result<Bee> {
         let count = db.count_bees().await?;
         if count >= 99 {
             // todo: error too many bees
         }
         let bees = db.get_bees().await?;
         let mut available_ids = (1..99).collect::<Vec<u8>>();
-        for bee in bees.flatten() {
+
+        for bee in bees {
             available_ids.retain(|id| *id != bee.id);
         }
-        let new_id = available_ids.first().ok_or("No id to use".to_string())?;
+
+        let new_id = available_ids
+            .first()
+            .ok_or(anyhow::anyhow!("No id to use"))?;
 
         // todo: continue implementation
 
-        Ok(Bee {
+        let bee = Bee {
             id: *new_id,
             neighborhood: String::new(),
             reserve_doubling: false,
-        })
+        };
+        db.add_bee(bee.clone()).await?;
+        Ok(bee)
     }
 
     pub fn name(&self) -> String {
         format!("node_{}", Self::format_id(self.id))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::services::db_service::MockDbService;
+
+    #[tokio::test]
+    async fn test_format_id() {
+        assert_eq!(Bee::format_id(5), "05");
+        assert_eq!(Bee::format_id(99), "99"); // since u8 can't be over 255, but the function accepts any u8
+    }
+
+    #[tokio::test]
+    async fn test_get_name() {
+        let bee = Bee {
+            id: 5,
+            neighborhood: String::new(),
+            reserve_doubling: false,
+        };
+        assert_eq!(bee.name(), "node_05");
+    }
+
+    #[tokio::test]
+    async fn test_create_first_bee() {
+        let mock = MockDbService::default();
+
+        let new_bee = Bee::create(&mock).await.unwrap();
+        assert_eq!(new_bee.id, 1);
+        assert_eq!(mock.count_bees().await.unwrap(), 1);
+    }
+
+    #[tokio::test]
+    async fn test_create_with_existing_ids() {
+        let mock = MockDbService::default();
+
+        mock.add_bee(Bee {
+            id: 1,
+            neighborhood: String::new(),
+            reserve_doubling: false,
+        })
+        .await
+        .unwrap();
+
+        mock.add_bee(Bee {
+            id: 3,
+            neighborhood: String::new(),
+            reserve_doubling: false,
+        })
+        .await
+        .unwrap();
+
+        let new_bee = Bee::create(&mock).await.unwrap();
+        assert_eq!(new_bee.id, 2);
+        assert_eq!(mock.count_bees().await.unwrap(), 3);
+    }
+
+    #[tokio::test]
+    async fn test_create_no_available_id() {
+        let mock = MockDbService::default();
+
+        for id in 1..99 {
+            mock.add_bee(Bee {
+                id,
+                neighborhood: String::new(),
+                reserve_doubling: false,
+            })
+            .await
+            .unwrap();
+        }
+        let result = Bee::create(&mock).await;
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_create_all_ids_taken() {
+        let mock = MockDbService::default();
+
+        for id in 1..=99 {
+            mock.add_bee(Bee {
+                id,
+                neighborhood: String::new(),
+                reserve_doubling: false,
+            })
+            .await
+            .unwrap();
+        }
+        let result = Bee::create(&mock).await;
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_create_with_available_gap() {
+        let mock = MockDbService::default();
+
+        mock.add_bee(Bee {
+            id: 1,
+            neighborhood: String::new(),
+            reserve_doubling: false,
+        })
+        .await
+        .unwrap();
+        mock.add_bee(Bee {
+            id: 3,
+            neighborhood: String::new(),
+            reserve_doubling: false,
+        })
+        .await
+        .unwrap();
+
+        let new_bee = Bee::create(&mock).await.unwrap();
+        assert_eq!(new_bee.id, 2); // Should pick the first missing ID
+        assert_eq!(mock.count_bees().await.unwrap(), 3);
     }
 }
--- src/services/db_service.rs
+++ src/services/db_service.rs
@@ -1,8 +1,20 @@
 use crate::bee::Bee;
+use anyhow::Error;
+use anyhow::Result;
+use async_trait::async_trait;
 use polodb_core::bson::doc;
-use polodb_core::{ClientCursor, Collection, CollectionT, Database};
+use polodb_core::{Collection, CollectionT, Database};
+use std::collections::VecDeque;
 use std::sync::Arc;
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+
+#[async_trait]
+pub trait BeeDatabase {
+    async fn add_bee(&self, bee: Bee) -> Result<()>;
+    async fn add_bees(&self, bees: Vec<Bee>) -> Result<()>;
+    async fn count_bees(&self) -> Result<u64>;
+    async fn get_bees(&self) -> Result<Vec<Bee>>;
+}
 
 #[derive(Clone)]
 pub struct DbService {
@@ -24,34 +36,82 @@ impl DbService {
     async fn get_bees_col_read(&self) -> Collection<Bee> {
         self.db.read().await.collection::<Bee>("bees")
     }
+}
 
-    pub async fn add_bee(&self, bee: Bee) -> Result<(), String> {
+#[async_trait]
+impl BeeDatabase for DbService {
+    async fn add_bee(&self, bee: Bee) -> Result<()> {
         let collection = self.get_bees_col_write().await;
-        collection.insert_one(bee).map_err(|err| err.to_string())?;
+        collection.insert_one(bee)?;
         Ok(())
     }
 
-    pub async fn add_bees(&self, bees: Vec<Bee>) -> Result<(), String> {
+    async fn add_bees(&self, bees: Vec<Bee>) -> Result<()> {
         let collection = self.get_bees_col_write().await;
-        collection
-            .insert_many(bees)
-            .map_err(|err| err.to_string())?;
+        collection.insert_many(bees)?;
         Ok(())
     }
 
-    pub async fn count_bees(&self) -> Result<u64, String> {
+    async fn count_bees(&self) -> Result<u64> {
         let collection = self.get_bees_col_read().await;
-        collection.count_documents().map_err(|err| err.to_string())
+        collection.count_documents().map_err(Error::from)
     }
 
-    pub async fn get_bees(&self) -> Result<ClientCursor<Bee>, String> {
+    async fn get_bees(&self) -> Result<Vec<Bee>> {
         let collection = self.get_bees_col_read().await;
-        collection
+        let cursor = collection
             .find(doc! {})
             .sort(doc! {
                 "id": 1
             })
             .run()
-            .map_err(|err| err.to_string())
+            .map_err(Error::from)?;
+
+        let mut bees = Vec::new();
+        for result in cursor {
+            let bee = result.map_err(Error::from)?;
+            bees.push(bee);
+        }
+        Ok(bees)
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct MockDbService {
+    db: Arc<RwLock<VecDeque<Bee>>>,
+}
+
+impl MockDbService {
+    async fn get_bees_col_write(&self) -> RwLockWriteGuard<'_, VecDeque<Bee>> {
+        self.db.write().await
+    }
+
+    async fn get_bees_col_read(&self) -> RwLockReadGuard<'_, VecDeque<Bee>> {
+        self.db.read().await
+    }
+}
+
+#[async_trait]
+impl BeeDatabase for MockDbService {
+    async fn add_bee(&self, bee: Bee) -> Result<()> {
+        let mut queue = self.get_bees_col_write().await;
+        queue.push_back(bee);
+        Ok(())
+    }
+
+    async fn add_bees(&self, bees: Vec<Bee>) -> Result<()> {
+        let mut queue = self.get_bees_col_write().await;
+        queue.extend(bees);
+        Ok(())
+    }
+
+    async fn count_bees(&self) -> Result<u64> {
+        let queue = self.get_bees_col_read().await;
+        Ok(queue.len() as u64)
+    }
+
+    async fn get_bees(&self) -> Result<Vec<Bee>> {
+        let queue = self.get_bees_col_read().await;
+        Ok(queue.clone().make_contiguous().to_vec())
     }
 }
