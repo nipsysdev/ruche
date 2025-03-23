61bc0fc527c2f26073569c561f28f9570c196c07
Xavier Saliniere <bonjour@xaviers.sh>
2025-03-23 07:06:55 -0400

    feature(docker): start implementing new_bee_container

--- src/core/docker.rs
+++ src/core/docker.rs
@@ -1,13 +1,22 @@
+use anyhow::Result;
 use async_trait::async_trait;
-use bollard::Docker as BollarDocker;
+use bollard::{
+    container::{Config, CreateContainerOptions},
+    secret::HostConfig,
+    Docker as BollarDocker,
+};
 use dyn_clone::DynClone;
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 use tokio::sync::Mutex;
+
+use crate::models::bee::BeeInfo;
 
 dyn_clone::clone_trait_object!(BeeDocker);
 
 #[async_trait]
-pub trait BeeDocker: DynClone + Send + Sync {}
+pub trait BeeDocker: DynClone + Send + Sync {
+    async fn new_bee_container(&self, bee: BeeInfo) -> Result<()>;
+}
 
 #[derive(Clone)]
 pub struct Docker {
@@ -24,4 +33,29 @@ impl Docker {
     }
 }
 
-impl BeeDocker for Docker {}
+#[async_trait]
+impl BeeDocker for Docker {
+    async fn new_bee_container(&self, bee: BeeInfo) -> Result<()> {
+        let docker = self.docker.lock().await;
+
+        let path_mount = format!("{}:{}", bee.data_dir.to_string_lossy(), "/home/bee/.bee");
+
+        let options = Some(CreateContainerOptions {
+            name: bee.name,
+            platform: None,
+        });
+
+        let config = Config {
+            image: Some(bee.image),
+            host_config: Some(HostConfig {
+                binds: Some(vec![path_mount]),
+                ..Default::default()
+            }),
+            ..Config::default()
+        };
+
+        docker.create_container(options, config).await?;
+
+        Ok(())
+    }
+}
